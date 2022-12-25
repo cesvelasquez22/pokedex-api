5785f7daadbc3d2fb4f95a0f9cbb55bd91a75996
Cesar Velásquez Córdova <ces.velasquez@hotmail.com>
2022-12-25 16:25:18 -0600

    + do http request (axios)

--- src/seed/seed.service.ts
+++ src/seed/seed.service.ts
@@ -1,8 +1,21 @@
 import { Injectable } from '@nestjs/common';
+import axios, { AxiosInstance } from 'axios';
+import { PokeResponse } from './seed.types';
 
 @Injectable()
 export class SeedService {
-  execute() {
-    return { message: 'Executed!' };
+  private readonly axios: AxiosInstance = axios;
+  async execute() {
+    const { data } = await this.axios.get<PokeResponse>(
+      'https://pokeapi.co/api/v2/pokemon?limit=10',
+    );
+
+    for (const { name, url } of data.results) {
+      const segments = url.split('/');
+      const pokeId = +segments[segments.length - 2];
+      console.log({ name, pokeId });
+    }
+
+    return data.results;
   }
 }
